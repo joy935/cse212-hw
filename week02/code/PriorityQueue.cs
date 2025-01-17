68a900a37e8bc3d1975a65d60d452967913d217d
joy935 <tupaiv@gmail.com>
2025-01-17 12:08:29 +0100

    finished Priority Queue

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -24,14 +24,15 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index < _queue.Count; index++) // changed _queue.Count - 1 to _queue.Count because we need to check the last item
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority) // changed >= to > to get the first item with the highest priority
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex); // added this part to remove the item from the queue
         return value;
     }
 
--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,118 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Create a queue with the following people and priorities:
+    // Sue, 2; Bob, 16; Tim, 5.
+    // Expected Result: Bob, Tim, Sue, 
+    // Defect(s) Found: Dequeue didn't have a function that would remove the item from the queue. 
+    // Added _queue.RemoveAt(highPriorityIndex) to remove the item from the queue.
+    public void TestPriorityQueue_EnqueueDequeue()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var sue = new PriorityItem("Sue", 2);
+        var bob = new PriorityItem("Bob", 16);
+        var tim = new PriorityItem("Tim", 5);
+
+        PriorityItem[] expectedResult = { bob, tim, sue };
+
+        var pq = new PriorityQueue();
+        pq.Enqueue(bob.Value, bob.Priority);
+        pq.Enqueue(tim.Value, tim.Priority);
+        pq.Enqueue(sue.Value, sue.Priority);
+
+        int i = 0;
+        while (i < expectedResult.Length)
+        {
+            var expectedValue = expectedResult[i];
+            var actualValue = pq.Dequeue(); 
+
+            Assert.AreEqual(expectedValue.Value, actualValue);
+            i++;
+        }
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Create a queue with the following people and priorities:
+    // Lui, 1; Sue, 2; Bob, 16; Tim, 2.
+    // Expected Result: Bob, Sue, Tim
     // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    public void TestPriorityQueue_SamePriority()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var lui = new PriorityItem("Lui", 1);
+        var sue = new PriorityItem("Sue", 2);
+        var bob = new PriorityItem("Bob", 16);
+        var tim = new PriorityItem("Tim", 2);
+
+        PriorityItem[] expectedResult = { bob, sue, tim, lui }; 
+
+        var pq = new PriorityQueue();
+        pq.Enqueue("Lui", 1);
+        pq.Enqueue("Sue", 2);
+        pq.Enqueue("Bob", 16);
+        pq.Enqueue("Tim", 2);
+
+        int i = 0;
+        while (i < expectedResult.Length) {
+            Console.WriteLine(pq.ToString());
+            var expectedValue = expectedResult[i];
+            Console.WriteLine("Expected: " +expectedValue);
+            var actualValue = pq.Dequeue();
+            Console.WriteLine("Actual: " +actualValue);
+
+            Assert.AreEqual(expectedValue.Value, actualValue);
+            i++;
+        }
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: Create a queue with similar priorities and dequeue until the queue is empty:
+    // Tim, 10; Sue, 2; Bob, 16; Lui, 2; Leo, 16;
+    // Expected Result: Bob, Leo, Tim, Sue, Lui
+    // Defect(s) Found: None
+    public void TestPriorityQueue_SamePriorities()
+    {
+        var tim = new PriorityItem("Tim", 10);
+        var sue = new PriorityItem("Sue", 2);
+        var bob = new PriorityItem("Bob", 16);
+        var lui = new PriorityItem("Lui", 2);
+        var leo = new PriorityItem("Leo", 16);
+
+        PriorityItem[] expectedResult = { bob, leo, tim, sue, lui };
+
+        var pq = new PriorityQueue();
+        pq.Enqueue("Tim", 10);
+        pq.Enqueue("Sue", 2);
+        pq.Enqueue("Bob", 16);
+        pq.Enqueue("Lui", 2);
+        pq.Enqueue("Leo", 16);
+
+        int i = 0;
+        while (i < expectedResult.Length) {
+            var expectedValue = expectedResult[i];
+            var actualValue = pq.Dequeue();
+
+            Assert.AreEqual(expectedValue.Value, actualValue);
+            i++;
+        }
+    }
+
+    [TestMethod]
+    // Scenario: Try to dequeue from an empty queue.
+    // Expected Result: Exception thrown with message "The queue is empty."
+    // Defect(s) Found: None
+    public void TestPriorityQueue_EmptyQueue()
+    {
+        var pq = new PriorityQueue();
+        
+        try {
+            pq.Dequeue();
+            Assert.Fail("Exception not thrown.");
+
+        } catch (InvalidOperationException e) {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        } catch (AssertFailedException) {
+            throw;
+        } catch (Exception e) {
+            Assert.Fail("Wrong exception thrown.", e.GetType(), e.Message);
+        }
+    }
 }
